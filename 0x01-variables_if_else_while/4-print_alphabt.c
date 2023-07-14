fb47a0a44b4fee77aab587084c46d97623379015
marcusjayweather <marcusjnr61@gmail.com>
2023-07-14 14:46:08 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main:  Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 
 	for (except = 'a'; except <= 'z'; except++)
 	{
-		if (except != 'q' && except != 'e')
+		if (except != q && except != e)
 		putchar(except);
 	}
 	putchar('\n');
